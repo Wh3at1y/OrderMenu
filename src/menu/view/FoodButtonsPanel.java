8d15b9847c7e01f252404e8fad9fe5d16d608eb9
Sam Montoya <smon7958@740s-pl-118.local>
2016-02-01 12:17:50 -0700

    Added Recipe and Stuff in Food Panel

--- src/menu/view/FoodButtonsPanel.java
+++ src/menu/view/FoodButtonsPanel.java
@@ -6,29 +6,43 @@ import javax.swing.*;
 
 public class FoodButtonsPanel extends JPanel
 	{
-		private GridLayout baseLayout;
-		private JButton firstButton;
+		private SpringLayout baseLayout;
+		
+		private JTextArea showPrice;
+		private JButton secretMenuButton;
 		
 		public FoodButtonsPanel()
 		{
-			baseLayout = new GridLayout();
-			firstButton = new JButton("Middle Panel");
+			baseLayout = new SpringLayout();
+			
+			showPrice = new JTextArea();
+			secretMenuButton = new JButton("Secret Menu");
+			
 			
 			buildPanel();
+			buildPlacements();
 		}
 		
 		private void buildPanel()
 		{
 			setPreferredSize(new Dimension(600,600));
 			setBackground(new Color(156, 246, 210));
-			
 			setLayout(baseLayout);
-			add(firstButton);
+			
+			add(showPrice);
+			add(secretMenuButton);
 		}
 		
 		private void buildPlacements()
 		{
-			
+			baseLayout.putConstraint(SpringLayout.NORTH, secretMenuButton, 0, SpringLayout.NORTH, showPrice);
+			baseLayout.putConstraint(SpringLayout.WEST, secretMenuButton, 10, SpringLayout.WEST, this);
+			baseLayout.putConstraint(SpringLayout.SOUTH, secretMenuButton, 0, SpringLayout.SOUTH, showPrice);
+			baseLayout.putConstraint(SpringLayout.EAST, secretMenuButton, 273, SpringLayout.WEST, this);
+			baseLayout.putConstraint(SpringLayout.NORTH, showPrice, -87, SpringLayout.SOUTH, this);
+			baseLayout.putConstraint(SpringLayout.WEST, showPrice, -273, SpringLayout.EAST, this);
+			baseLayout.putConstraint(SpringLayout.SOUTH, showPrice, -10, SpringLayout.SOUTH, this);
+			baseLayout.putConstraint(SpringLayout.EAST, showPrice, -10, SpringLayout.EAST, this);
 		}
 		
 		private void buildListeners()
--- src/menu/view/RecipePanel.java
+++ src/menu/view/RecipePanel.java
@@ -8,13 +8,23 @@ import javax.swing.*;
 public class RecipePanel extends JPanel
 	{
 		private SpringLayout baseLayout;
-		private JButton firstButton;
+		
+		private JTextArea showRecipe;
+		private JButton printButton;
+		private JButton deleteButton;
 		
 		public RecipePanel()
 		{
 			baseLayout = new SpringLayout();
-			firstButton = new JButton("This in the top panel");
+			
+			showRecipe = new JTextArea();
+			showRecipe.setEditable(false);
+			
+			printButton = new JButton("Print Recipe");
+			deleteButton = new JButton("Delete Item");
+			
 			buildPanel();
+			buildPlacements();
 		}
 		
 		private void buildPanel()
@@ -22,12 +32,26 @@ public class RecipePanel extends JPanel
 			setPreferredSize(new Dimension(300,900));
 			setBackground(new Color(190, 234, 249));
 			setLayout(baseLayout);
-			add(firstButton);
+			
+			add(showRecipe);
+			add(printButton);
+			add(deleteButton);
 		}
 		
 		private void buildPlacements()
 		{
-			
+			baseLayout.putConstraint(SpringLayout.NORTH, deleteButton, 605, SpringLayout.NORTH, this);
+			baseLayout.putConstraint(SpringLayout.WEST, deleteButton, -142, SpringLayout.EAST, this);
+			baseLayout.putConstraint(SpringLayout.SOUTH, deleteButton, -230, SpringLayout.SOUTH, this);
+			baseLayout.putConstraint(SpringLayout.EAST, deleteButton, -10, SpringLayout.EAST, this);
+			baseLayout.putConstraint(SpringLayout.NORTH, printButton, -295, SpringLayout.SOUTH, this);
+			baseLayout.putConstraint(SpringLayout.WEST, printButton, 10, SpringLayout.WEST, this);
+			baseLayout.putConstraint(SpringLayout.SOUTH, printButton, -230, SpringLayout.SOUTH, this);
+			baseLayout.putConstraint(SpringLayout.EAST, printButton, 142, SpringLayout.WEST, this);
+			baseLayout.putConstraint(SpringLayout.EAST, showRecipe, 0, SpringLayout.EAST, deleteButton);
+			baseLayout.putConstraint(SpringLayout.SOUTH, showRecipe, -6, SpringLayout.NORTH, printButton);
+			baseLayout.putConstraint(SpringLayout.NORTH, showRecipe, 10, SpringLayout.NORTH, this);
+			baseLayout.putConstraint(SpringLayout.WEST, showRecipe, 10, SpringLayout.WEST, this);
 		}
 		
 		private void buildListeners()
